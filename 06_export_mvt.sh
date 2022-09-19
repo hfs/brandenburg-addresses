f7b8f42e975397ea775d0a7b15ef512967a06209
Hermann Schwarting <hfs@gmx.de>
2022-09-19 22:54:03 +0200

    Bug: Clear output directory only once

--- 06_export_mvt.sh
+++ 06_export_mvt.sh
@@ -28,8 +28,6 @@ export_tiles() {
 	local max_zoom="$3"
 	local extent_table="${4:-$1}"
 
-	echo ">>> Delete old 'tiles' directory"
-	rm -rf tiles
 	echo ">>> Exporting table $table for zoom levels $min_zoom to $max_zoom"
 	tiles=$(mktemp -t addresses.XXXXXX)
 	iterate_tiles $extent_table $min_zoom $max_zoom > $tiles
@@ -45,6 +43,8 @@ export_tiles() {
 
 # Zoom levels are mutually exclusive, because tiles from all tables are merged
 # in a single directory
+echo ">>> Delete old 'tiles' directory"
+rm -rf tiles
 export_tiles geoadr_overview 0 12 geoadr_aggregation
 export_tiles geoadr_point 13 13 geoadr_matches
 export_tiles geoadr_detail 14 14 geoadr_matches
