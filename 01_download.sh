2e8844e79fc561201c334cdcc769aadceb737b99
Hermann Schwarting <hfs@gmx.de>
2022-11-06 16:40:07 +0100

    Fail if required input file missing

--- 01_download.sh
+++ 01_download.sh
@@ -8,6 +8,7 @@ if [ ! -r geoadr.gpkg ]; then
     echo "Please go to https://geobroker.geobasis-bb.de/ and download the data"
     echo "set “Georeferenzierte Adresse” in GeoPackage format. Please provide"
     echo "it as file data/geoadr.gpkg."
+    exit 1
 fi
 echo ">>> Downloading OpenStreetMap dump for '$REGION_PATH'"
 wget "https://download.geofabrik.de/$REGION_PATH-latest.osm.pbf" \
