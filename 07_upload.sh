9137cb822f9010ad6fbaf091a318b41ff6a81e2a
Hermann Schwarting <hfs@gmx.de>
2022-12-07 21:48:25 +0100

    Upload script: Add progress messages
    I had some problems with the automated data processing where the process
    got killed in the middle of this upload script. Added some messages to
    have an idea next time where it failed.

--- 07_upload.sh
+++ 07_upload.sh
@@ -9,14 +9,19 @@ export OSM_DATE=$(date -d @$(stat --format %Y data/$REGION-latest.osm.pbf) +%Y-%
 export CHANGE_DATE=$(cat data/timestamp.txt)
 export TILES_DATE=$(date -d @$(stat --format %Y tiles/) +%Y-%m-%dT%H:%M:%S%z)
 
-[ -d ../brandenburg_addresses_site/tiles/ ]
+[ -d ../brandenburg_addresses_site/ ]
+echo ">>> Clear website staging directory"
 rm -rf ../brandenburg_addresses_site/*
+echo ">>> Copy generated tiles and website into site directory"
 cp -a --reflink=auto tiles/ site/* .github/ style.json mapbox-gl-style.json ../brandenburg_addresses_site/
+echo ">>> Variable substitution"
 cat site/de/timestamp.md | envsubst > ../brandenburg_addresses_site/de/timestamp.md
 cat site/en/timestamp.md | envsubst > ../brandenburg_addresses_site/en/timestamp.md
 cd ../brandenburg_addresses_site/
+echo ">>> git commit"
 git add -u .
 git add .
 git commit -m "Temporary commit"
 git reset $(git commit-tree HEAD^{tree} -m "Tiles generated on $TILES_DATE" -m "Addresses from: $ADDRESS_DATE" -m "OpenStreetMap dump from: $OSM_DATE")
+echo ">>> Push to GitHub"
 git push -f
